fcf55ead617f01b1b6ac7e7c2d3e67098aa33542
Beryl-742 <Berylawuor742@gmail.com>
2025-06-15 22:56:01 +0300

    Scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+// Declaring a variable using Var
+var customerName = 'bob' ;
+
+//Function to uppercase the global customerName
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+//Declaring a global variable from inside a function
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+//Overwrite the global bestCustomer variable
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+//Declare a constant in global scope
+const leastFavoriteCustomer = 'Karen';
+//Attempt to change a constant
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Steve';
+}
